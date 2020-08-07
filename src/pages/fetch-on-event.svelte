25cd88dd5ac030107c6ae8ce7511bb0bba236839
wktdev <wktdev@pop-os.localdomain>
2020-08-07 12:36:23 -0700

    save

---
+++ src/pages/fetch-on-event.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { onMount } from 'svelte';
+
+	import {fetchData} from './_get_data.svelte';
+
+  // import Button from '@smui/button';
+   let apiData = [];
+   $:reactiveApiData = apiData;
+
+
+   async function getData(){
+      let result = await fetchData("https://api.github.com/users/wktdev");
+      apiData = Object.values(result);
+   } 
+  
+</script>
+
+<div>
+
+    <button on:click = {getData}>CLICK ME </button>
+
+    {#each reactiveApiData as item}
+     <li>{item}</li>
+    {/each}
+
+</div>
---
+++ src/pages/fetch-on-load.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+
+	import {fetchData} from './_get_data.svelte';
+
+  // import Button from '@smui/button';
+   let apiData = [];
+   $:reactiveApiData = apiData;
+   onMount(async () => {
+   	let result = await fetchData("https://api.github.com/users/wktdev");
+   	apiData = Object.values(result);
+   })
+  
+</script>
+
+<div>
+
+    {#each reactiveApiData as item}
+     <li>{item}</li>
+    {/each}
+
+</div>
+
+<!--  <script>
+  import Navigation from './_components/Navigation.svelte';
+</script>
+<div>
+  <Navigation/>
+</div> -->
